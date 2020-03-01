148ec453c2197dae1ef08cd21a8ede2ace701edd
cmhayden <haydencallum4@gmail.com>
2020-03-01 21:54:33 +0000

    create a gender subset and display the number of male and female participants

--- src/main.r
+++ src/main.r
@@ -1,4 +1,7 @@
 # Read in the data 
 dataset <- read.csv("/Users/cmhayden/interestingProjects/Visualizing-Inequalities-in-Life-Expectancy/Data/UN Data - Life expectancy at birth.txt", sep=";")
 
+# Create a gender subset and display how many male and female data records are stored
+subset_gender <- subset(dataset, select=c("Subgroup"))
+summary(subset_gender)
 
